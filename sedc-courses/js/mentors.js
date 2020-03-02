fb077827328f578ec37be6b401f6a3bc2e3ba26e
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-03-02 20:38:13 +0100

    Homework: mentors

---
+++ sedc-courses/js/mentors.js
@@ -0,0 +1,18 @@
+//Domasna:
+//1. Treba da ja povrzete ovaa skripta so mentors stranata.
+//2. Vo ovaj file treba da se napravi funkcija imenuvana getAllMentors, kojasto
+//   ke gi zeme od server site mentori i ke gi stavi vo varijablata mentors kako niza od objekti i potoa ke ja
+//   povika funkcijata drawMentorsInHTMl i kako parametar ke se isprati nizata mentors.
+
+//   Serverot ke vi vrati objekt so vnatre objekti mentori, ke treba da go premapirate objektot vo niza.
+//   URL-to na koe se naogjaat podatocite e https://sedcohrid.firebaseio.com/mentors.json
+//3. Drugata funkcija drawMentorsInHTML prima niza od mentori i treba za sekoj od mentorite vo nizata da
+//   se kreira bootstrap card kako primerot so kursevite i da se stavi vo divot so id mentors.
+//   (Probajte da go reiskoristite kodot za kreiranje na card-ovi od index.js)
+//4. Dopolnitelno treba i koga ke odite so mausot vrz nekoja od card-ovite
+
+var mentors;
+
+function getAllMentors() {}
+
+function drawMentorsInHTML(arrayOfMentors) {}
