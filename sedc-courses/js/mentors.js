b4d7fba7bb82535fcc76e7b92e0b3484b63bd473
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-03-03 13:44:22 +0100

    Homework class 12

--- sedc-courses/js/mentors.js
+++ sedc-courses/js/mentors.js
@@ -9,7 +9,12 @@
 //3. Drugata funkcija drawMentorsInHTML prima niza od mentori i treba za sekoj od mentorite vo nizata da
 //   se kreira bootstrap card kako primerot so kursevite i da se stavi vo divot so id mentors.
 //   (Probajte da go reiskoristite kodot za kreiranje na card-ovi od index.js)
-//4. Dopolnitelno treba i koga ke odite so mausot vrz nekoja od card-ovite
+//4. Na klik na kopceto show more treba da se nosi userot kon stranata mentor.html i kako query parametar treba
+//   da se isprati id-to od userot. (example: mentor.html?id=igorp)
+//5. Vo mentor.html treba da se zemi idto od query stringot i da se napravi povik do serverot na URL:
+//   https://sedcohrid.firebaseio.com/mentors/igorp.json   (ova igorp normalno bi bilo zameneto so id-to)
+//6. So podatocite koi ke gi vrati serverot (povekje informacii ima samo za userot so id igorp) treba
+//   da napravite nekoja opis, po vas izbor ostavam. (kako primer moze da ja zemite slikata so vi ja isprativ)
 
 var mentors;
 
